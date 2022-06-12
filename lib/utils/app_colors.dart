25679320ad29c9a26e55a5498ecab3f69b9037e3
sonu.kumar <sonu.kumar@teliolabs.com>
2022-06-12 15:51:06 +0530

    Register and login ui

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+const kColorPrimary = const Color(0xff0085FF);
+const kColorPrimaryDark = const Color(0xff0085FF);
+const kColorAccent = const Color(0xffFF7262);
+const kColorPrimaryLight = const Color(0xff0085FF);
+const kColorPrimaryText = const Color(0xff033623);
+const kColorSecondaryText = const Color(0xfffff1);
+const kColorDivider = const Color(0xffBDBDBD);
+const kColorBackground = const Color(0xfffafafa);
+const kColorError = const Color(0xfff44336);
+const kColorSuccess = const Color(0xff4caf50);
+const kColorWarning = const Color(0xffffeb3b);
+const kColorInfo = const Color(0xff2196f3);
+const kColorInverseBackground = const Color(0xff000000);
+const kColorInverseText = const Color(0xffffffff);
+const kColorFacebook = const Color(0xff3b5998);
+const kColorGoogle = const Color(0xffdb4437);
+const kColorTwitter = const Color(0xff1da1f2);
